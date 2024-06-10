e77dbf01a5950cab19e565664f7bbabbabb564d7
Esteban Candia <E.candxa@gmail.com>
2024-06-10 00:26:23 -0400

    Build inicial proyecto back + docker-compose

---
+++ backend/OOO-backend/src/main/java/org/ooo/backend/OooBackendApplication.java
@@ -0,0 +1,13 @@
+package org.ooo.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OooBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OooBackendApplication.class, args);
+	}
+
+}
---
+++ backend/OOO-backend/src/test/java/org/ooo/backend/OooBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package org.ooo.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OooBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
