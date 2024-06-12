f0a3a0cddbdc28a9a6076c7557919f1194e6dded
Claudio Espinoza <claudioyugen@gmail.com>
2024-06-11 21:21:23 -0400

    Fix<frontend>: Corrige el tipado del router

--- frontend/src/router/index.ts
+++ frontend/src/router/index.ts
@@ -1,19 +1,20 @@
-import { createRouter, createWebHistory } from 'vue-router';
+import { createRouter, createWebHistory, RouteRecordRaw } from 'vue-router';
+
+const routes: Array<RouteRecordRaw> = [
+  {
+    path: '/',
+    redirect: '/landing',
+  },
+  {
+    path: '/landing',
+    name: 'landing',
+    component: () => import('../pages/LandingPage.vue'),
+  },
+];
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
-
-  routes: [
-    {
-      path: '/',
-      redirect: '/landing',
-    },
-    {
-      path: '/landing',
-      name: 'landing',
-      component: () => import('../pages/LandingPage.vue'),
-    },
-  ],
+  routes,
 });
 
 export default router;
