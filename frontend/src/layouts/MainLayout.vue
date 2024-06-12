6f8c8dc6c938d32cc44d3a46496176ad6e644239
Claudio Espinoza <claudioyugen@gmail.com>
2024-06-11 22:30:10 -0400

    Feat<frontend>: Agrega estandarizacion del css

---
+++ frontend/src/layouts/MainLayout.vue
@@ -0,0 +1,10 @@
+<script setup lang="ts">
+</script>
+
+<template>
+    <main>
+        <slot> </slot>
+
+    </main>
+
+</template>
--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -1,4 +1,5 @@
-import './style.css';
+import './assets/style/variables.css';
+import './assets/style/style.css';
 
 import { createApp } from 'vue';
 import { createPinia } from 'pinia';
