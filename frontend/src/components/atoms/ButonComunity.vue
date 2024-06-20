f6d5959986676a5bae9c9f5d4722b9a207a24ac1
Claudio Espinoza <claudioyugen@gmail.com>
2024-06-19 23:09:53 -0400

    Feat<frontend>: Hover de un boton

--- frontend/src/components/atoms/ButonComunity.vue
+++ frontend/src/components/atoms/ButonComunity.vue
@@ -1,7 +1,8 @@
 <script setup lang="ts">
 
 interface Props {
-    url: string
+    url: string,
+    link: string
 }
 
 const props = defineProps<Props>();
@@ -10,20 +11,33 @@ const props = defineProps<Props>();
 
 <template>
     <button>
-        <img class="button-img" :src="props.url" alt="">
-
+        <a href=""><img class="button-img" :src="props.url" alt=""></a>
     </button>
+
 </template>
 
 <style scoped>
 button {
     width: 20%;
     height: 70%;
-    background-color: var(--color-naranja);
+    background-color: var(--color-morado);
     border-radius: 1vh;
     border: none;
     border: var(border);
+    overflow: hidden;
+    transition: .3s cubic-bezier(.6, .4, 0, 1), transform .15s ease;
 }
+
+button:hover {
+    background-color: var(--color-naranja);
+    transform: scale(1.1);
+}
+
+button:active {
+    background-color: var(--color-naranja);
+    transform: scale(0.95);
+}
+
 
 .button-img {
     width: 70%;
