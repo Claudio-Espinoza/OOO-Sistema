72ec13590b97f79f359ff1820b3cd2504f8e77bf
Claudio Espinoza <claudioyugen@gmail.com>
2024-06-22 12:04:35 -0400

    Feat<frontend>: Termina la vista de desafio

---
+++ frontend/src/components/atoms/ButonFilter.vue
@@ -0,0 +1,53 @@
+<script setup lang="ts">
+
+interface Props {
+    content: string
+    state: boolean
+}
+
+const props = defineProps<Props>();
+
+</script>
+
+<template>
+    <button :class="'button-' + props.state">{{ props.content }}</button>
+</template>
+
+<style scoped>
+.button-false {
+    border: none;
+
+    width: 28%;
+    height: 50%;
+
+    border-radius: 0.5vw;
+    border: var(--borde);
+    font-size: 0.8rem;
+    font-weight: 600;
+    cursor: pointer;
+}
+
+.button-true {
+    border: none;
+
+    width: 28%;
+    height: 50%;
+
+    border-radius: 0.5vw;
+
+    border: 0.4vh solid var(--color-morado);
+    font-size: 0.8rem;
+    font-weight: 600;
+    color: #fff;
+    background-color: var(--color-morado);
+    cursor: pointer;
+}
+
+button:hover {
+    transform: scale(1.05);
+}
+
+button:active {
+    transform: scale(0.95);
+}
+</style>
---
+++ frontend/src/components/atoms/ChallengerCard.vue
@@ -0,0 +1,65 @@
+<script setup lang="ts">
+interface Props {
+    title?: String,
+    course?: String,
+    type?: string
+}
+
+const props = defineProps<Props>();
+</script>
+
+<template>
+    <div class="card">
+        <div :class="'card-img-' + props.type">
+            <img v-if="props.type === 'java'" src="/src/assets/icon/java-blanco.png" alt="">
+            <img v-if="props.type === 'python'" src="/src/assets/icon/python-blanco.png" alt="">
+        </div>
+
+        <div>
+            <h4>{{ props.title }}</h4>
+            <small> {{ props.course }} </small>
+        </div>
+    </div>
+</template>
+
+<style scoped>
+.card {
+    width: 31%;
+    height: 15%;
+    border: var(--borde);
+    display: flex;
+    align-items: center;
+    justify-content: space-around;
+    flex-direction: row;
+    border-radius: 0.5vw;
+
+}
+
+small {
+    font-style: italic;
+}
+
+.card-img-java,
+.card-img-python {
+    height: 80%;
+    width: 18%;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    border-radius: 0.5vw;
+}
+
+.card-img-java {
+    background-color: var(--color-naranja);
+}
+
+.card-img-python {
+    background-color: var(--color-morado);
+}
+
+.card-img-java>img,
+.card-img-python>img {
+    width: 70%;
+    height: auto;
+}
+</style>
