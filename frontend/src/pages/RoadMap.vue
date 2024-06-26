1bf8ad407607ba7b3d9a901ea173267ff7f26390
Claudio Espinoza <claudioyugen@gmail.com>
2024-06-26 11:28:44 -0400

    Feat<frontend>: Crea la maquetacion del roadmap

---
+++ frontend/src/pages/RoadMap.vue
@@ -0,0 +1,28 @@
+<script setup lang="ts">
+import MainLayout from '../layouts/MainLayout.vue';
+import { useRouter } from 'vue-router';
+
+const router = useRouter();
+
+</script>
+
+<template>
+    <MainLayout>
+        <article class="container-main">
+
+        </article>
+    </MainLayout>
+</template>
+
+<style scoped>
+.container-main {
+    width: 95%;
+    height: 100%;
+    background-color: aqua;
+
+    display: flex;
+    flex-direction: row;
+    align-items: center;
+    justify-content: center;
+}
+</style>
