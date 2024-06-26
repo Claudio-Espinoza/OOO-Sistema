32394747c9256ba3cb151745aface3b5a452a4fb
Claudio Espinoza <claudioyugen@gmail.com>
2024-06-26 13:03:54 -0400

    Feat<frontend>: Agrega el listado de hitos en el roadmap

---
+++ frontend/src/components/atoms/ButonRoadMap.vue
@@ -0,0 +1,94 @@
+<script setup lang="ts">
+
+interface Props {
+    title: string,
+    position: number,
+    type: string
+}
+
+const props = defineProps<Props>();
+
+</script>
+
+<template>
+    <aside :class="'button-' + props.type">
+        <div :class="'button-position-' + props.type">
+            {{ props.position }}
+        </div>
+        <p class="button-title">
+            {{ props.title }}
+        </p>
+        <button class="button-action">
+            Ingresar
+        </button>
+    </aside>
+</template>
+
+<style scoped>
+* {
+    margin: 0;
+    padding: 0;
+}
+
+.button-java {
+    width: 90%;
+    height: 8%;
+
+    background-color: var(--background-color);
+    display: flex;
+    flex-direction: row;
+
+    align-items: center;
+    justify-content: space-between;
+
+    color: var(--font-color);
+    font-family: var(--font-family);
+
+    padding-left: 0.5vw;
+    padding-right: 0.5vw;
+    transition: all 0.2s;
+
+    border: var(--borde);
+    border-radius: 0.5vw;
+}
+
+.button-position-java {
+    display: flex;
+    align-items: center;
+    justify-content: center;
+
+    color: #fff;
+    font-weight: 500;
+    font-size: 1.2rem;
+    width: 6%;
+    height: 70%;
+
+    border-radius: 0.2vw;
+    background-color: var(--color-naranja);
+}
+
+.button-title {
+    color: var(--font-color);
+    font-size: 1rem;
+    font-weight: 400;
+    width: 75%;
+}
+
+.button-action {
+    background-color: var(--background-color);
+    border: var(--borde);
+    cursor: pointer;
+    height: 60%;
+    width: 15%;
+    border-radius: 0.2vw;
+}
+
+.button-action:hover {
+    transform: scale(1.05);
+}
+
+.button-action:active {
+
+    transform: scale(0.95);
+}
+</style>
--- frontend/src/pages/RoadMap.vue
+++ frontend/src/pages/RoadMap.vue
@@ -1,6 +1,15 @@
 <script setup lang="ts">
 import MainLayout from '../layouts/MainLayout.vue';
+import ButonRoadMap from '../components/atoms/ButonRoadMap.vue';
 
+const milestones = [
+    { name: "PSeint", type: 'java' },
+    { name: "Proyectos de java", type: 'java' },
+    { name: "Sintaxis", type: 'java' },
+    { name: "Estructura de datos", type: 'java' },
+    { name: "Clases", type: 'java' },
+    { name: "Objetos", type: 'java' },
+];
 
 </script>
 
@@ -11,9 +20,9 @@ import MainLayout from '../layouts/MainLayout.vue';
                 <h2 class="roadmap-list-title">Conocimientos hitos de
                     <strong class="roadmap-list-decoration"> Introducción a <br> programación</strong>
                 </h2>
-
                 <section class="roadmap-list--button">
-
+                    <ButonRoadMap v-for="(item, index) in milestones" :key="index" :title="item.name"
+                        :position="index + 1" :type="item.type" />
                 </section>
             </article>
             <aside class="roadmap-description"></aside>
@@ -34,13 +43,13 @@ import MainLayout from '../layouts/MainLayout.vue';
 
 .roadmap-list {
     display: flex;
-    width: 100%;
+    width: 80%;
     height: 100%;
     flex-direction: column;
 }
 
 .roadmap-list-title {
-    margin-top: 2%;
+    margin-top: 4%;
     text-align: left;
     font-size: 1.2rem;
     font-weight: 400;
@@ -58,17 +67,18 @@ import MainLayout from '../layouts/MainLayout.vue';
 }
 
 .roadmap-list--button {
+    margin-top: 3%;
     display: flex;
     width: 100%;
     height: 100%;
-    background-color: brown;
     flex-direction: column;
+    gap: 2vh;
 }
 
 
 .roadmap-description {
     display: flex;
-    width: 80%;
+    width: 100%;
     height: 100%;
     background-color: aqua;
     flex-direction: column;
