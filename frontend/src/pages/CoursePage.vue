f33bd655e1124e4f41b6097bfaeca8958323b474
Claudio Espinoza <claudioyugen@gmail.com>
2024-06-26 11:47:49 -0400

    Refactor<frontend>: Limpia los componentes de legados

--- frontend/src/pages/CoursePage.vue
+++ frontend/src/pages/CoursePage.vue
@@ -1,14 +1,28 @@
 <script setup lang="ts">
 import MainLayout from '../layouts/MainLayout.vue';
 import ButonCurse from '../components/atoms/ButonCurse.vue';
+import { useRouter } from 'vue-router';
+
+const router = useRouter();
+
+const changePage = (id: string): void => {
+    const ruta = `/roadmap/${id}`;
+    router.push(ruta);
+}
+
 </script>
 
 <template>
     <MainLayout>
         <article class="container-main">
-            <ButonCurse type="java" content="Introducción a Programación" />
-            <ButonCurse type="java" content="Programación Orientada a Objeto" />
-            <ButonCurse type="python" content="Taller de Programación" />
+            <ButonCurse type="java" content="Introducción a Programación"
+                @click="changePage('Introducción a Programación')" />
+
+            <ButonCurse type="java" content="Programación Orientada a Objeto"
+                @click="changePage('Introducción a Programación')" />
+
+            <ButonCurse type="python" content="Taller de Programación"
+                @click="changePage('Introducción a Programación')" />
         </article>
     </MainLayout>
 </template>
