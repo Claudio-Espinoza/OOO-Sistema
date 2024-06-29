b63566b7267275b8bfaa7bda80cb4eb0a9a8b765
Claudio Espinoza <claudioyugen@gmail.com>
2024-06-29 10:54:56 -0400

    Feat<frontend>: Añade nueva estructura de layout

---
+++ frontend/src/layouts/SourceLayout.vue
@@ -0,0 +1,48 @@
+<script setup lang="ts">
+import NavBar from '../components/organisms/NavBar.vue';
+import { useRoute } from 'vue-router';
+import { ref, Ref, watch } from 'vue';
+
+const route = useRoute();
+let titleGeneral: Ref<string> = ref(formatTitle(route.path));
+
+watch(() => route.path, (newPath) => {
+    titleGeneral.value = formatTitle(newPath);
+});
+
+const handleButtonClick = (title: string) => {
+    titleGeneral.value = title;
+};
+
+function formatTitle(path: string): string {
+    const parts = path.split('/');
+    if (parts.length > 1) {
+        const secondPart = parts[1];
+        return secondPart.charAt(0).toUpperCase() + secondPart.slice(1);
+    }
+    return '';
+}
+</script>
+
+<template>
+    <NavBar @clicked="handleButtonClick" :seccion="titleGeneral" />
+    <main class="container">
+        <slot>
+
+
+        </slot>
+    </main>
+
+</template>
+
+<style scoped>
+.container {
+    background-color: var(--background-color);
+    width: 100%;
+    height: 100%;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+
+}
+</style>
---
+++ frontend/src/pages/SourcePage.vue
@@ -0,0 +1,23 @@
+<script setup lang="ts">
+import MainLayout from '../layouts/MainLayout.vue';
+import SliderHelper from '../components/molecules/SliderHelper.vue';
+</script>
+
+<template>
+    <MainLayout>
+        <article class="container-main">
+            <SliderHelper />
+        </article>
+    </MainLayout>
+</template>
+
+<style scoped>
+.container-main {
+    width: 95%;
+    height: 100%;
+
+    display: flex;
+    justify-content: center;
+    align-items: center;
+}
+</style>
