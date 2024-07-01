9aa5a51dbca8b94efbc57a4d5c83ca2158dfd52a
Esteban Candia <E.candxa@gmail.com>
2024-07-01 01:38:14 -0400

    Feat<Back>: Creacion servicio para añadir puntuaciones a las lecciones

--- backend/OOO-backend/src/main/java/org/ooo/backend/service/LeccionService.java
+++ backend/OOO-backend/src/main/java/org/ooo/backend/service/LeccionService.java
@@ -7,6 +7,7 @@ import org.springframework.stereotype.Service;
 
 import java.util.List;
 import java.util.NoSuchElementException;
+import java.util.Optional;
 
 @Service
 @RequiredArgsConstructor
@@ -14,15 +15,38 @@ public class LeccionService {
 
     private final LeccionRepository leccionRepository;
 
-    public List<Leccion> getAllLeccionByCurso(int idCurso){
+    public List<Leccion> obtenerTodasLasLeccionesByCurso(int idCurso){
 
         List<Leccion> lecciones = leccionRepository.findAllByCursoId(idCurso);
 
         if(!lecciones.isEmpty()){
             return lecciones;
         } else {
-            throw new NoSuchElementException("No se encontraron cursos");
+            throw new NoSuchElementException("No se encontraron lecciones");
         }
+    }
+
+
+    public void añadirPuntuacionLeccion(int idLeccion, boolean positiva){
+
+        Optional<Leccion> leccion = leccionRepository.findById(idLeccion);
+
+        if(leccion.isPresent() ){
+
+            Leccion leccionTemporal = leccion.get();
+
+            if (positiva){
+                leccionTemporal.setPuntuacionPositiva(leccionTemporal.getPuntuacionPositiva() + 1);
+            }else {
+                leccionTemporal.setPuntuacionNegativa(leccionTemporal.getPuntuacionNegativa() + 1);
+            }
+
+            leccionRepository.save(leccionTemporal);
+        } else {
+            throw new NoSuchElementException("No se encontro la leccion");
+        }
+
+
     }
 
 }
