45a250cafd3c6d911108bc8bad05363704da790a
Claudio Espinoza <claudioyugen@gmail.com>
2024-07-01 11:21:37 -0400

    Feat<frontend>: Agrega las interfaces de curso

--- frontend/src/model/Course.ts
+++ frontend/src/model/Course.ts
@@ -2,3 +2,19 @@ export interface ButonCurseProps {
   type: string;
   content: string;
 }
+
+export interface ICurso {
+  id: number;
+  nombre: string;
+  descripcion: string;
+  lenguaje: string;
+  lecciones: ILeccion[];
+}
+
+export interface ILeccion {
+  id: number;
+  nombre: string;
+  autor: string;
+  puntuacionPositiva: number;
+  puntuacionNegativa: number;
+}
