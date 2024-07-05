b63aa539852e5e7c011eb06f9266136ba99de056
Esteban Candia <E.candxa@gmail.com>
2024-07-04 21:27:54 -0400

    Feat<Back>: creacion servicio y endpoint para obtener el rol de un ususario

--- backend/OOO-backend/src/main/java/org/ooo/backend/controller/UserController.java
+++ backend/OOO-backend/src/main/java/org/ooo/backend/controller/UserController.java
@@ -1,2 +1,30 @@
-package org.ooo.backend.controller;public class UserController {
+package org.ooo.backend.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.ooo.backend.service.UserService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.server.ResponseStatusException;
+
+import java.util.NoSuchElementException;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/user")
+public class UserController {
+
+    private final UserService userService;
+
+    @GetMapping("obtener-rol")
+    public ResponseEntity<String> obtenerRol(@RequestParam String nombreUsuario){
+        try {
+            return ResponseEntity.ok(userService.obtenerRolUsuario(nombreUsuario));
+        } catch (NoSuchElementException e) {
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND, e.getMessage(), e);
+        }
+
+    }
+
+
 }
--- backend/OOO-backend/src/main/java/org/ooo/backend/service/UserService.java
+++ backend/OOO-backend/src/main/java/org/ooo/backend/service/UserService.java
@@ -1,2 +1,29 @@
-package org.ooo.backend.service;public class UserService {
+package org.ooo.backend.service;
+
+import lombok.RequiredArgsConstructor;
+import org.ooo.backend.model.Usuario;
+import org.ooo.backend.repository.UsuarioRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.NoSuchElementException;
+import java.util.Optional;
+
+@Service
+@RequiredArgsConstructor
+public class UserService {
+
+    private final UsuarioRepository usuarioRepository;
+
+
+    public String obtenerRolUsuario(String nombreUsuario){
+        Optional<Usuario> usuario = usuarioRepository.findByNombre(nombreUsuario);
+
+        if(usuario.isPresent()){
+            return usuario.get().getTipoUsuario().toString();
+        } else {
+            throw new NoSuchElementException(" Usuario no encontrado con Nombre: " + nombreUsuario);
+        }
+    }
+
+
 }
